0a0affabde84e247e7f894835be5e74f0d2e2066
Puja Rochmansyah <pujarchmnsyh21@gmail.com>
2020-03-15 09:06:24 +0700

    Halaman MM

--- Apss/src/apss/Multimedia.java
+++ Apss/src/apss/Multimedia.java
@@ -7,14 +7,14 @@ package apss;
 
 /**
  *
- * @author Asus
+ * @author pujarchmnsyh
  */
-public class Jurusan extends javax.swing.JFrame {
+public class Multimedia extends javax.swing.JFrame {
 
     /**
-     * Creates new form Jurusan
+     * Creates new form Multimedia
      */
-    public Jurusan() {
+    public Multimedia() {
         initComponents();
     }
 
@@ -25,30 +25,56 @@ public class Jurusan extends javax.swing.JFrame {
      */
     int dragxmouse;
     int dragymouse;
-    Multimedia b = new Multimedia();
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lblClose = new javax.swing.JLabel();
+        close = new javax.swing.JLabel();
+        cinema = new javax.swing.JLabel();
+        duaD = new javax.swing.JLabel();
+        tigaD = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        lblTkj = new javax.swing.JLabel();
-        lblRpl = new javax.swing.JLabel();
-        lblMm = new javax.swing.JLabel();
-        lblBackground = new javax.swing.JLabel();
+        mm = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
         getContentPane().setLayout(null);
 
-        lblClose.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        lblClose.addMouseListener(new java.awt.event.MouseAdapter() {
+        close.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        close.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                lblCloseMouseClicked(evt);
+                closeMouseClicked(evt);
             }
         });
-        getContentPane().add(lblClose);
-        lblClose.setBounds(617, 9, 25, 23);
+        getContentPane().add(close);
+        close.setBounds(620, 10, 20, 20);
+
+        cinema.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        cinema.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                cinemaMouseClicked(evt);
+            }
+        });
+        getContentPane().add(cinema);
+        cinema.setBounds(50, 110, 160, 190);
+
+        duaD.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        duaD.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                duaDMouseClicked(evt);
+            }
+        });
+        getContentPane().add(duaD);
+        duaD.setBounds(250, 110, 140, 190);
+
+        tigaD.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        tigaD.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tigaDMouseClicked(evt);
+            }
+        });
+        getContentPane().add(tigaD);
+        tigaD.setBounds(440, 110, 160, 190);
 
         jLabel1.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
             public void mouseDragged(java.awt.event.MouseEvent evt) {
@@ -63,39 +89,38 @@ public class Jurusan extends javax.swing.JFrame {
         getContentPane().add(jLabel1);
         jLabel1.setBounds(0, 0, 610, 40);
 
-        lblTkj.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        lblTkj.addMouseListener(new java.awt.event.MouseAdapter() {
+        mm.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mmimage/mm.png"))); // NOI18N
+        mm.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                lblTkjMouseClicked(evt);
+                mmMouseClicked(evt);
             }
         });
-        getContentPane().add(lblTkj);
-        lblTkj.setBounds(461, 97, 122, 154);
-
-        lblRpl.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        lblRpl.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                lblRplMouseClicked(evt);
-            }
-        });
-        getContentPane().add(lblRpl);
-        lblRpl.setBounds(64, 97, 122, 154);
-
-        lblMm.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(lblMm);
-        lblMm.setBounds(260, 97, 122, 154);
-
-        lblBackground.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jurusanimage/bg_jurusan.png"))); // NOI18N
-        getContentPane().add(lblBackground);
-        lblBackground.setBounds(0, 0, 650, 350);
+        getContentPane().add(mm);
+        mm.setBounds(0, 0, 650, 350);
 
         setSize(new java.awt.Dimension(650, 350));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void lblCloseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCloseMouseClicked
+    private void closeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_closeMouseClicked
         dispose();
-    }//GEN-LAST:event_lblCloseMouseClicked
+    }//GEN-LAST:event_closeMouseClicked
+
+    private void cinemaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cinemaMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cinemaMouseClicked
+
+    private void duaDMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_duaDMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_duaDMouseClicked
+
+    private void tigaDMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tigaDMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tigaDMouseClicked
+
+    private void mmMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_mmMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mmMouseClicked
 
     private void jLabel1MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseDragged
         int x = evt.getXOnScreen();
@@ -109,14 +134,6 @@ public class Jurusan extends javax.swing.JFrame {
         dragxmouse = evt.getX();
         dragymouse = evt.getY();
     }//GEN-LAST:event_jLabel1MousePressed
-
-    private void lblRplMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblRplMouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_lblRplMouseClicked
-
-    private void lblTkjMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblTkjMouseClicked
-        b.setVisible(true);
-    }//GEN-LAST:event_lblTkjMouseClicked
 
     /**
      * @param args the command line arguments
@@ -135,30 +152,30 @@ public class Jurusan extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Jurusan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Multimedia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Jurusan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Multimedia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Jurusan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Multimedia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Jurusan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Multimedia.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Jurusan().setVisible(true);
+                new Multimedia().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel cinema;
+    private javax.swing.JLabel close;
+    private javax.swing.JLabel duaD;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel lblBackground;
-    private javax.swing.JLabel lblClose;
-    private javax.swing.JLabel lblMm;
-    private javax.swing.JLabel lblRpl;
-    private javax.swing.JLabel lblTkj;
+    private javax.swing.JLabel mm;
+    private javax.swing.JLabel tigaD;
     // End of variables declaration//GEN-END:variables
 }
